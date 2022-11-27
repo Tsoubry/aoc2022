180c4ea44a8de10405a7c5995eb95bd7c9225b2d
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-11-27 14:15:55 +0100

    update generate script

--- generate.sh
+++ generate.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Create cookie file, fill in with cookie from the network tools once logged in on adventofcode.com
+
 [[ $# -eq 0 ]] && echo "Usage: ./generate.sh day"
 
 echo "🦀 DAY $1"
 
+AOCYEAR=2022
+URL=https://adventofcode.com/$AOCYEAR/day/$1
+
 cargo new --vcs none --bin --edition 2021 --name day$1 day$1
 
-
+curl -f $URL/input -H "cookie: $(cat cookie)" > day$1/input.txt 2> /dev/null
