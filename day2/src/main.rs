0308f21886548ad06228a15e2e577f897e5c892d
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-02 08:52:08 +0100

    working solution day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,159 @@
+#[derive(PartialEq, Clone)]
+enum RPS {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(PartialEq, Clone)]
+enum Strategy {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl RPS {
+    fn parse(text: &str) -> (RPS, RPS) {
+        let mut chars = text.chars();
+        let opponent = match chars.next().unwrap() {
+            'A' => Self::Rock,
+            'B' => Self::Paper,
+            'C' => Self::Scissors,
+            _ => unreachable!()
+        };
+        let you = match chars.last().unwrap() {
+            'X' => Self::Rock,
+            'Y' => Self::Paper,
+            'Z' => Self::Scissors,
+            _ => unreachable!()
+        };
+        (opponent, you)
+    }
+
+    fn shape_score(&self) -> i64 {
+        match self {
+            RPS::Rock => 1,
+            RPS::Paper => 2,
+            RPS::Scissors => 3,
+        }
+    }
+
+    fn total_score((opponent, you): (RPS, RPS)) -> (i64, i64) {
+        let win_score = if opponent == you {
+            (3 , 3)
+        } else if opponent == RPS::Rock {
+            if you == RPS::Paper {
+                (0, 6)
+            } else { (6, 0) }
+        } else if opponent == RPS::Paper {
+            if you == RPS::Scissors {
+                (0, 6)
+            } else { (6, 0) }
+        } else {
+            if you == RPS::Rock {
+                (0, 6)
+            } else { (6, 0) }
+        };
+
+        (win_score.0 + opponent.shape_score(), win_score.1 + you.shape_score())
+    }
+
+    fn parse2(text: &str) -> (RPS, Strategy) {
+        let mut chars = text.chars();
+        let opponent = match chars.next().unwrap() {
+            'A' => Self::Rock,
+            'B' => Self::Paper,
+            'C' => Self::Scissors,
+            _ => unreachable!()
+        };
+        let you = match chars.last().unwrap() {
+            'X' => Strategy::Lose,
+            'Y' => Strategy::Draw,
+            'Z' => Strategy::Win,
+            _ => unreachable!()
+        };
+        (opponent, you)
+    }
+
+    fn total_score2((opponent, your_strategy): (RPS, Strategy)) -> (i64, i64) {
+
+        let win_rps = if opponent == RPS::Rock {
+            RPS::Paper
+        } else if opponent == RPS::Paper {
+            RPS::Scissors
+        } else { RPS::Rock };
+
+        let lose_rps = if opponent == RPS::Rock {
+            RPS::Scissors
+        } else if opponent == RPS::Paper {
+            RPS::Rock
+        } else { RPS::Paper };
+
+        match your_strategy {
+            Strategy::Draw => RPS::total_score((opponent.clone(), opponent)),
+            Strategy::Lose => RPS::total_score((opponent, lose_rps)),
+            Strategy::Win => RPS::total_score((opponent, win_rps)),
+        }
+    }
+}
+
+fn parse_data(data: &str) -> Vec<(RPS, RPS)> {
+    data.lines().map(|line| RPS::parse(line)).collect()
+}
+
+fn parse_data2(data: &str) -> Vec<(RPS, Strategy)> {
+    data.lines().map(|line| RPS::parse2(line)).collect()
+}
+
+fn answer1(data: Vec<(RPS, RPS)>) -> i64 {
+    data
+        .into_iter()
+        .map(|game| RPS::total_score(game))
+        .map(|result| result.1)
+        .sum()
+}
+
+fn answer2(data: Vec<(RPS, Strategy)>) -> i64 {
+    data
+    .into_iter()
+    .map(|game| RPS::total_score2(game))
+    .map(|result| result.1)
+    .sum()
+}
+
+fn main() {
+    let input_data = parse_data(include_str!("../input.txt"));
+
+    println!("Answer of part 1 is: {}", answer1(input_data));
+
+    let input_data2 = parse_data2(include_str!("../input.txt"));
+
+    println!("Answer of part 2 is: {}", answer2(input_data2));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_answer1() {
+        let test_data = r#"A Y
+B X
+C Z
+"#;
+
+        let parsed_data = parse_data(test_data);
+        assert_eq!(15, answer1(parsed_data));
+    }
+
+    #[test]
+    fn test_answer2() {
+        let test_data = r#"A Y
+B X
+C Z
+"#;
+
+        let parsed_data = parse_data2(test_data);
+        assert_eq!(12, answer2(parsed_data));
+    }
+}
