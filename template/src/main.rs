c24b46a4f9d9898fb2c4d17f0aba9a4515db8b05
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-04 15:20:13 +0100

    small changes template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,24 +1,27 @@
-fn import_data(data: &str) -> Vec<_> {
+type Parsed = ();
+
+
+fn import_data(data: &str) -> Vec<Parsed> {
     data.lines().map(|line| parse(line)).collect()
 }
 
-fn parse(line: &str) -> () {
+fn parse(line: &str) -> Parsed {
     todo!()
 }
 
-fn answer_part1(data: Vec<_>) -> i64 {
+fn answer_part1(data: Vec<Parsed>) -> i64 {
     todo!();
 }
 
-fn answer_part2(data: Vec<_>) -> i64 {
-    todo!();
-}
+// fn answer_part2(data: Vec<Parsed>) -> i64 {
+//     todo!();
+// }
 
 fn main() {
     let input_data = import_data(include_str!("../input.txt"));
 
     println!("Answer of part 1 is: {}", answer_part1(input_data.clone()));
-    println!("Answer of part 2 is: {}", answer_part2(input_data));
+    // println!("Answer of part 2 is: {}", answer_part2(input_data));
 }
 
 #[cfg(test)]
@@ -27,6 +30,7 @@ mod tests {
     use super::*;
 
     fn test_data() -> &'static str {
+        todo!();
         r#"
 "#
     }
@@ -37,11 +41,11 @@ mod tests {
         assert_eq!(, answer_part1(input_data));
     }
 
-    #[test]
-    fn test_answer2() {
-        let input_data = import_data(test_data());
-        assert_eq!(, answer_part2(input_data));
-    }
+    // #[test]
+    // fn test_answer2() {
+    //     let input_data = import_data(test_data());
+    //     assert_eq!(, answer_part2(input_data));
+    // }
 
     #[test]
     fn playground() {
