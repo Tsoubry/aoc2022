9888597751fc26a4ab8641a3f76747049af047ee
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-07 21:22:28 +0100

    progress

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -66,46 +66,39 @@ impl Default for Fs {
 }
 
 impl Fs {
+    fn get_current_directory(&mut self) -> &mut Dir {
+        self.dirs
+            .iter_mut()
+            .find(|d| d.name == self.cursor)
+            .unwrap()
+    } // this is wrong! Just use a hashmap
+
     fn run_commands(&mut self, data: Vec<String>) {
         for line in data {
             match parse_command(&line) {
                 Operation::Ls => (),
                 Operation::ReturnDir(dir_name) => {
                     let new_directory = Dir::new(dir_name, 0, Some(self.cursor.clone()), vec![]);
-                    let current_directory = self
-                    .dirs
-                    .iter_mut()
-                    .find(|d| d.name == self.cursor)
-                    .unwrap();
+                    let current_directory = self.get_current_directory();
                     current_directory.dirs.push(new_directory);
                 }
                 Operation::ReturnSize(size) => {
-                    let current_dir = self
-                        .dirs
-                        .iter_mut()
-                        .find(|d| d.name == self.cursor)
-                        .unwrap();
-                    current_dir.filesize += size;
+                    let current_directory = self.get_current_directory();
+                    current_directory.filesize += size;
                 }
                 Operation::Cd(dir_name) => {
                     match dir_name.as_str() {
                         ".." => {
-                            let upper_directory = self
-                                .dirs
-                                .iter()
-                                .find(|d| d.name == self.cursor)
-                                .unwrap()
-                                .upper_directory
-                                .clone();
-                            self.cursor = upper_directory.unwrap();
+                            let current_directory = self.get_current_directory();
+                            self.cursor = current_directory.upper_directory.clone().unwrap();
                         }
                         "/" => {
                             self.cursor = "/".to_string();
                         }
                         x_directory => {
-
                             println!("x dir: {:?}", &self.dirs);
-                            let cd_directory = self
+                            let current_directory = self.get_current_directory();
+                            let cd_directory = current_directory
                                 .dirs
                                 .iter_mut()
                                 .find(|d| d.name == x_directory)
@@ -119,30 +112,27 @@ impl Fs {
     }
 
     fn calculate_result(&self) -> u64 {
-
-        
         calculate_dir_sizes(&self.dirs, 0)
-
     }
 }
 
 fn calculate_dir_sizes(sub_dirs: &Vec<Dir>, size: u64) -> u64 {
-
     for dir in sub_dirs {
-        let recursive_dir_sizes =  calculate_dir_sizes(&dir.dirs, dir.filesize + size);
+        let recursive_dir_sizes = calculate_dir_sizes(&dir.dirs, dir.filesize + size);
         if recursive_dir_sizes <= 100000 {
             println!("recursive: {}", recursive_dir_sizes);
-            return recursive_dir_sizes
+            return recursive_dir_sizes;
         } else {
-            return 0
+            return 0;
         }
     }
 
     if size <= 100000 {
         println!("size: {}", size);
         size
-    } else { 0 }
-
+    } else {
+        0
+    }
 }
 
 fn answer_part1(data: Vec<String>) -> u64 {
