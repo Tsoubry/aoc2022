9f84ab10da2dea99023e0bcdd180162fbae83f7a
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-15 20:53:20 +0100

    not going to solve this today

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -21,40 +21,48 @@ fn answer_part1(data: Vec<(Coordinate, Coordinate)>, y_level: isize) -> usize {
 
         let y_range = sensor_range - (sensor.y - y_level).abs();
 
-        // println!("y range: {}", y_range);
-
         if y_range > 0 {
             let x_start = sensor.x - y_range;
             let x_end = sensor.x + y_range + 1;
-
-            // println!("x start: {}, x end: {}", x_start, x_end);
 
             for x_point in min(x_start, x_end)..max(x_start, x_end) {
                 places_marked.insert(x_point);
             }
         }
-
-        // let mut iter = min(sensor.y, beacon.y)..(max(sensor.y, beacon.y) + 1);
-
-        // if iter.contains(&y_level) {
-
-        //     let diff = max(sensor.y, beacon.y) - y_level;
-
-        // }
     }
 
     let mut v = places_marked.iter().collect::<Vec<_>>();
     v.sort();
 
-    // println!("hashset: {:?}", v);
-    // println!("remove beacons: {:?}", &beacon_locations);
-
     places_marked.len() - beacon_locations.len()
 }
 
-// fn answer_part2(data: Vec<Parsed>) -> i64 {
+fn answer_part2(data: Vec<(Coordinate, Coordinate)>, x_max: usize, y_max: usize) -> usize {
 
-// }
+    // sensors
+
+    // start with range of sensors
+
+    for (sensor, beacon) in data {
+        
+    }
+
+
+
+    let x_min = 0;
+    let y_min = 0;
+
+    // beacon between 0 and
+    // 4_000_000
+
+    // tuning frequency: x * 4_000_000 + y
+
+    let distress_beacon_x = 20;
+    let distress_beacon_y = 10;
+
+    distress_beacon_x * 4_000_000 + distress_beacon_y
+
+}
 
 fn main() {
     let input_data = import_data(include_str!("../input.txt"));
@@ -80,11 +88,11 @@ mod tests {
         assert_eq!(26, answer_part1(input_data, 10));
     }
 
-    // #[test]
-    // fn test_answer2() {
-    //     let input_data = import_data(TEST_DATA);
-    //     assert_eq!(, answer_part2(input_data));
-    // }
+    #[test]
+    fn test_answer2() {
+        let input_data = import_data(TEST_DATA);
+        assert_eq!(56000011, answer_part2(input_data, 20, 20));
+    }
 
     #[test]
     fn playground() {}
