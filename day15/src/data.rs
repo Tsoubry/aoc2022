a83ff50638ee9e4ca671a2ad99563d9017e51ee5
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-15 18:25:04 +0100

    solution part 1 day 15

--- day15/src/data.rs
+++ day15/src/data.rs
@@ -11,24 +11,31 @@ pub fn import_data(data: &str) -> Vec<(Coordinate, Coordinate)> {
 }
 
 fn text_to_coordinate(text: &str) -> Coordinate {
-
     let re = Regex::new(r".+at x=(\-?\d+), y=(-?\d+)").unwrap();
     let captures = re.captures(text).expect("no captures found");
 
-    let x = captures.get(1).expect("first capture group").as_str().parse::<isize>().expect("parse to isize");
-    let y = captures.get(2).expect("second capture group").as_str().parse::<isize>().expect("parse to isize");
+    let x = captures
+        .get(1)
+        .expect("first capture group")
+        .as_str()
+        .parse::<isize>()
+        .expect("parse to isize");
+    let y = captures
+        .get(2)
+        .expect("second capture group")
+        .as_str()
+        .parse::<isize>()
+        .expect("parse to isize");
 
     Coordinate::new(x, y)
-
 }
 
 fn parse(line: &str) -> (Coordinate, Coordinate) {
     let mut iter = line
-    .split_terminator(": ")
-    .map(|text|text_to_coordinate(text));
+        .split_terminator(": ")
+        .map(|text| text_to_coordinate(text));
 
     (iter.next().unwrap(), iter.next().unwrap())
-
 }
 
 pub const TEST_DATA: &str = r#"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -54,6 +61,6 @@ mod tests {
     #[test]
     fn test_parsing() {
         let input_data = import_data(TEST_DATA);
-        println!("{:?}", input_data);
+        // println!("{:?}", input_data);
     }
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,13 +1,56 @@
 pub mod data;
 
+use std::cmp::{max, min};
+use std::collections::HashSet;
+
 #[macro_use]
 extern crate derive_new;
 
 use crate::data::*;
 
-// fn answer_part1(data: Vec<Parsed>) -> i64 {
+fn answer_part1(data: Vec<(Coordinate, Coordinate)>, y_level: isize) -> usize {
+    let mut places_marked: HashSet<isize> = HashSet::new(); // x
+    let mut beacon_locations: HashSet<isize> = HashSet::new();
 
-// }
+    for (sensor, beacon) in data {
+        if beacon.y == y_level {
+            beacon_locations.insert(beacon.x);
+        }
+
+        let sensor_range = (sensor.x - beacon.x).abs() + (sensor.y - beacon.y).abs();
+
+        let y_range = sensor_range - (sensor.y - y_level).abs();
+
+        // println!("y range: {}", y_range);
+
+        if y_range > 0 {
+            let x_start = sensor.x - y_range;
+            let x_end = sensor.x + y_range + 1;
+
+            // println!("x start: {}, x end: {}", x_start, x_end);
+
+            for x_point in min(x_start, x_end)..max(x_start, x_end) {
+                places_marked.insert(x_point);
+            }
+        }
+
+        // let mut iter = min(sensor.y, beacon.y)..(max(sensor.y, beacon.y) + 1);
+
+        // if iter.contains(&y_level) {
+
+        //     let diff = max(sensor.y, beacon.y) - y_level;
+
+        // }
+    }
+
+    let mut v = places_marked.iter().collect::<Vec<_>>();
+    v.sort();
+
+    // println!("hashset: {:?}", v);
+    // println!("remove beacons: {:?}", &beacon_locations);
+
+    places_marked.len() - beacon_locations.len()
+}
 
 // fn answer_part2(data: Vec<Parsed>) -> i64 {
 
@@ -16,7 +59,12 @@ use crate::data::*;
 fn main() {
     let input_data = import_data(include_str!("../input.txt"));
 
-    // println!("Answer of part 1 is: {}", answer_part1(input_data.clone()));
+    let y_level = 2_000_000;
+
+    println!(
+        "Answer of part 1 is: {}",
+        answer_part1(input_data.clone(), y_level)
+    );
     // println!("Answer of part 2 is: {}", answer_part2(input_data));
 }
 
@@ -26,12 +74,11 @@ mod tests {
     use super::*;
     use crate::data::TEST_DATA;
 
-    // #[test]
-    // fn test_answer1() {
-    //     let input_data = import_data(TEST_DATA);
-    // for y = 10
-    //     assert_eq!(26, answer_part1(input_data));
-    // }
+    #[test]
+    fn test_answer1() {
+        let input_data = import_data(TEST_DATA);
+        assert_eq!(26, answer_part1(input_data, 10));
+    }
 
     // #[test]
     // fn test_answer2() {
@@ -40,7 +87,5 @@ mod tests {
     // }
 
     #[test]
-    fn playground() {
-        
-    }
+    fn playground() {}
 }
