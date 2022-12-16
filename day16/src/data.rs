fa31bf6cd2a8c729b27ec4624a990e0a1ad3b31d
Tanguy Soubry <tanguy.soubry@edpnet.be>
2022-12-16 09:58:03 +0100

    add setup and parsing

---
+++ day16/src/data.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
+
+#[derive(new, Debug, Clone)]
+pub struct Valve {
+    flow_rate: usize,
+    tunnels: Vec<String>,
+}
+
+pub fn import_data(data: &str) -> HashMap<String, Valve> {
+    data.lines().map(|line| parse(line)).collect()
+}
+
+pub fn parse(line: &str) -> (String, Valve) {
+
+    let re = Regex::new(r"Valve ([A-Z]+).+rate=(\d+); tunnels? leads? to valves? ([,A-Z ]+)").unwrap();
+
+    let caps = re.captures(&line).expect("no capture groups");
+
+    let name = caps.get(1).expect("first capture group").as_str().to_string();
+    let flow: usize = caps.get(2).expect("second capture group").as_str().parse().expect("problem with parsing flow rate");
+    let tunnels: Vec<_> = caps.get(3).expect("third capture group").as_str()
+        .split_terminator(", ")
+        .map(|valve| valve.to_string())
+        .collect()
+    ;
+
+    (name, Valve::new(flow, tunnels))
+    
+}
+
+pub const TEST_DATA: &str = r#"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+"#;
+
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parsing() {
+        let input_data = import_data(TEST_DATA);
+        // println!("{:?}", input_data);
+    }
+}
