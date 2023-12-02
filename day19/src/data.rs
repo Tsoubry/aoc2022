dda2994d045f6af3623e506da0d3cfc4557844eb
Tanguy Soubry <tanguy.soubry@edpnet.be>
2023-12-02 17:12:29 +0100

    add parser day 19

---
+++ day19/src/data.rs
@@ -0,0 +1,68 @@
+use regex::Regex;
+use derive_new::new;
+
+pub type AnswerDtype = i64;
+
+
+const PATTERN: &str = r"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.";
+
+#[derive(new, Debug, Copy, Clone)]
+pub struct Price {
+    pub ore: u32,
+    pub clay: u32,
+    pub obsidian: u32,
+}
+
+
+#[derive(new, Debug, Copy, Clone)]
+pub struct Blueprint {
+    pub number: u32,
+    pub ore_cost: Price,
+    pub clay_cost: Price,
+    pub obsidian_cost: Price,
+    pub geode_cost: Price,
+}
+
+
+
+pub fn import_data(data: &str) -> Vec<Blueprint> {
+    let re = Regex::new(PATTERN).unwrap();
+
+    data.lines().map(|line| parse(line, &re)).collect()
+}
+
+fn parse_str_to_num(s: &str) -> u32 {
+    s.parse().expect("couldn't parse str to u32")
+}
+
+pub fn parse(line: &str, re: &Regex) -> Blueprint {
+    let (_, [blueprint_num, ore_ore, clay_ore, obs_ore, obs_clay, geo_ore, geo_obs]) = re.captures(line).expect("capture with regex failed").extract();
+
+    Blueprint::new(
+        parse_str_to_num(blueprint_num),
+        Price::new(parse_str_to_num(ore_ore), 0, 0),
+        Price::new(parse_str_to_num(clay_ore), 0, 0),
+        Price::new(parse_str_to_num(obs_ore), parse_str_to_num(obs_clay), 0),
+        Price::new(parse_str_to_num(geo_ore), 0, parse_str_to_num(geo_obs)),
+    )
+
+}
+
+pub const TEST_DATA_1: &str = r#"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."#;
+pub const TEST_ANSWER_1: AnswerDtype = 33;
+
+pub const TEST_DATA_2: &str = TEST_DATA_1;
+pub const TEST_ANSWER_2: AnswerDtype = 0;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parsing() {
+        let input_data = import_data(TEST_DATA_1);
+        println!("{:?}", input_data);
+    }
+}
---
+++ day19/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod data;
+
+pub use crate::data::*;
+
+// pub fn answer_part1(data: Vec<Blueprint>) -> AnswerDtype {
+
+//     let minutes = 24;
+
+//     todo!()
+// }
+
+// pub fn answer_part2(data: Vec<Parsed>) -> AnswerDtype {
+//     todo!()
+// }
